d30d46e26e517ec1635a00eddd49a164afcd62ef
dch <daipanchi@gmail.com>
2013-12-27 17:20:56 +0900

    add ajax

--- app/controllers/star_controller.rb
+++ app/controllers/star_controller.rb
@@ -7,7 +7,8 @@ class StarController < ApplicationController
 
 			respond_to do |format|
 				if @star.save
-					format.html { redirect_to @micropost, notice: 'star was successfully created.' }
+					flash[:notice] = 'star was successfully created.'
+					format.html { redirect_to @micropost }
 					format.json { head :no_content }
 				else
 					format.html { render text: @star.errors.full_messages, status: :unprocessable_entity }
@@ -25,13 +26,13 @@ def add_star(postobj)
 	# current_user.stared_posts << postobj
 	@star = Star.new({id:1,user_id:1,micropost_id:1})
 	@star.save
-	return { :json => { :type => "add", :result => "success" } }
+	return { :json => { :type => "add", :result => "success" , :user => current_user.name }}
 end
 
 #スターを削除
 def remove_star(postobj)
 	current_user.stared_posts.delete(postobj)
-	return { :json => { :type => "remove", :result => "success" } }
+	return { :json => { :type => "remove", :result => "success" , :user => current_user.name }}
 end
 
 private
