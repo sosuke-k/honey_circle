5a242df6748d7df22184c9590f759d3629cf133f
dch <daipanchi@gmail.com>
2014-01-01 19:54:39 +0900

    show already-stared

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :micropost
-	validates :content, presence: true, length: { maximum: 140 }
+	validates :content, presence: true, length: { maximum: 10000 }
 	validates :user_id, presence: true
 end
--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -2,7 +2,7 @@ class Micropost < ActiveRecord::Base
 	belongs_to :user
 	has_many :comments
 	default_scope -> { order('created_at DESC') }
-	validates :content, presence: true, length: { maximum: 140 }
+	validates :content, presence: true, length: { maximum: 80 }
 	validates :user_id, presence: true
 
 	has_many :stars, :dependent => :destroy
