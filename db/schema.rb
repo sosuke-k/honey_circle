d1b3488cff2856c5318c3969a879c8c1766965d4
dch <daipanchi@gmail.com>
2014-01-06 06:28:01 +0900

    modify visual

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131229213236) do
+ActiveRecord::Schema.define(version: 20140105210438) do
 
   create_table "comments", force: true do |t|
     t.string   "content"
@@ -51,12 +51,21 @@ ActiveRecord::Schema.define(version: 20131229213236) do
   create_table "users", force: true do |t|
     t.string   "name"
     t.string   "email"
-    t.string   "provider"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "password_digest"
     t.string   "remember_token"
-    t.boolean  "admin",            default: false
+    t.boolean  "admin",                  default: false
+    t.string   "encrypted_password",     default: "",    null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,     null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.string   "provider"
     t.integer  "uid"
     t.string   "nickname"
     t.string   "image"
@@ -66,5 +75,6 @@ ActiveRecord::Schema.define(version: 20131229213236) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["remember_token"], name: "index_users_on_remember_token"
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
