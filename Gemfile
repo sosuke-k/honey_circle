6159e4c1c0d16deaf490efae62e35be0e5948561
SOSUKE KATO <sosuke@ubuntu.(none)>
2014-02-26 02:44:42 -0800

    Modify Gemfile

--- Gemfile
+++ Gemfile
@@ -2,6 +2,9 @@ source 'https://rubygems.org'
 ruby '2.0.0'
 #ruby-gemset=railstutorial_rails_4_0
 
+gem 'execjs'
+gem 'therubyracer', platforms: :ruby
+
 gem 'rails', '4.0.0'
 gem 'will_paginate', '3.0.4'
 gem 'bootstrap-will_paginate', '0.0.6'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140217162956) do
+ActiveRecord::Schema.define(version: 20140107022650) do
 
   create_table "comments", force: true do |t|
     t.string   "content"
@@ -37,7 +37,6 @@ ActiveRecord::Schema.define(version: 20140217162956) do
     t.string   "detail"
     t.integer  "desire"
     t.string   "interested_posts"
-    t.datetime "deleted_at"
   end
 
   add_index "microposts", ["user_id", "created_at"], name: "index_microposts_on_user_id_and_created_at"
@@ -52,33 +51,21 @@ ActiveRecord::Schema.define(version: 20140217162956) do
   create_table "users", force: true do |t|
     t.string   "name"
     t.string   "email"
+    t.string   "provider"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "password_digest"
     t.string   "remember_token"
-    t.boolean  "admin",                  default: false
-    t.string   "encrypted_password",     default: "",    null: false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,     null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.string   "provider"
+    t.boolean  "admin",            default: false
     t.integer  "uid"
     t.string   "nickname"
     t.string   "image"
     t.string   "stared_posts"
     t.string   "interested_posts"
     t.datetime "last_checked"
-    t.datetime "deleted_at"
   end
 
-  add_index "users", ["deleted_at"], name: "index_users_on_deleted_at"
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["remember_token"], name: "index_users_on_remember_token"
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
